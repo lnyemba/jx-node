906a3f5c504ac1fa7426f20790015cea1d53ae41
Steve L. Nyemba <nyemba@gmail.com>
2015-05-18 10:51:59 -0500

    updated math fucntions (repeat, covariance, correlations)

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ http		= require('httpsync')
 _couchdb  	= require('./lib/couch.js') ;
 _math		= require('./lib/math.js').math;
 _utils		= require('./lib/utils.js').utils;
+_ml		= require('./lib/ml.js').ml;
+
 jx = {} ;
 
 jx.math 	= _math ;
 jx.utils	= _utils;
+jx.ml		= _ml ;
 jx.couchdb      = {};
 jx.couchdb.instance= _couchdb.couchdb ;
 module.exports = jx;
